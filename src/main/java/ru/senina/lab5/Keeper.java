2ce5ecafd136f4cc190632a0f225d6fd5fe2059d
irybtsov <rybtsov_i_v@mail.ru>
2021-03-02 13:44:31 +0300

    bug with white spaces fixed

--- src/main/java/ru/senina/lab5/Keeper.java
+++ src/main/java/ru/senina/lab5/Keeper.java
@@ -27,11 +27,11 @@ public class Keeper {
     /**
      * Method to start a new collection and System.in reader
      */
-    public void start(){
+    public void start() {
         Parser parser = new Parser();
         try {
             File f = new File(filename);
-                collectionKeeper = parser.fromJsonToCollectionKeeper(parser.fromFileToString(filename));
+            collectionKeeper = parser.fromJsonToCollectionKeeper(parser.fromFileToString(filename));
         } catch (InvalidArgumentsException | NullPointerException | IOException e) {
             System.out.println("Filename is wrong. Collection will be empty.");
             collectionKeeper = new CollectionKeeper(new LinkedList<LabWork>());
@@ -54,128 +54,141 @@ public class Keeper {
         commandMap.put("print_descending", new PrintDescendingCommand(collectionKeeper, parser));
         try {
             terminal(parser, commandMap, "no file", 0);
-        }catch (IOException e){
+        } catch (IOException e) {
             throw new InvalidArgumentsException("You have entered wrong file name. Try the command again.");
         }
 
     }
 
     /**
-     *
-     * @param parser Parser instance
+     * @param parser     Parser instance
      * @param commandMap Map of all commands from string command to Command instance
-     * @param filename file to execute script
-     * @param level recursion level
+     * @param filename   file to execute script
+     * @param level      recursion level
      * @throws IOException throws if file has a wrong name
      */
     public void terminal(Parser parser, Map<String, Command> commandMap, String filename, int level) throws IOException {
-        if(level > 10 ){
+        if (level > 10) {
             System.out.println("You can't execute file recursively more then 10 times! The programme will be finished!");
             System.exit(1);
         }
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         File f = new File(filename);
-        if(f.exists() && !f.isDirectory() && Files.isReadable(f.toPath())) {
+        if (f.exists() && !f.isDirectory() && Files.isReadable(f.toPath())) {
             br = new BufferedReader(new InputStreamReader(new FileInputStream(filename)));
         }
 
         while (true) {
             try {
                 System.out.print("> ");
-                String[] line = br.readLine().split(" ");
-                if (commandMap.containsKey(line[0])) {
-                    boolean validCommand = true;
-                    Command command = commandMap.get(line[0]);
-                    command.setArgs(line);
-                    boolean commandIsReady = true;
-                    // TODO: переписать чтобы было без commandIsReady
-                    if (command instanceof ElementNeed) {
-                        commandIsReady = false;
-                        boolean exit = false;
-                        while (!exit) {
-                            try {
-                                //TODO: rewrite without code duplication
-                                LabWork element = new LabWork();
-                                System.out.println("You run a command, which needs LabWork element to be entered.");
+                String[] newline = br.readLine().split(" ");
+                String[] line = cleanLine(newline);
+                if (line.length > 0) {
+                    if (commandMap.containsKey(line[0])) {
+                        boolean validCommand = true;
+                        Command command = commandMap.get(line[0]);
+                        command.setArgs(line);
+                        boolean commandIsReady = true;
+                        // TODO: переписать чтобы было без commandIsReady
+                        if (command instanceof ElementNeed) {
+                            commandIsReady = false;
+                            boolean exit = false;
+                            while (!exit) {
+                                try {
+                                    //TODO: rewrite without code duplication
+                                    LabWork element = new LabWork();
+                                    System.out.println("You run a command, which needs LabWork element to be entered.");
 
-                                System.out.println("Enter element's name.");
-                                element.setName(br.readLine());
+                                    System.out.println("Enter element's name.");
+                                    element.setName(br.readLine());
 
-                                System.out.println("Enter coordinates. In first line x <= 74. In second y >= -47.");
-                                element.setCoordinates(new Coordinates(Integer.parseInt(br.readLine()), Long.parseLong(br.readLine())));
+                                    System.out.println("Enter coordinates. In first line x <= 74. In second y >= -47.");
+                                    element.setCoordinates(new Coordinates(Integer.parseInt(br.readLine()), Long.parseLong(br.readLine())));
 
-                                System.out.println("Enter minimal point.");
-                                element.setMinimalPoint(Float.parseFloat(br.readLine()));
+                                    System.out.println("Enter minimal point.");
+                                    element.setMinimalPoint(Float.parseFloat(br.readLine()));
 
-                                System.out.println("Enter element description.");
-                                element.setDescription(br.readLine());
+                                    System.out.println("Enter element description.");
+                                    element.setDescription(br.readLine());
 
-                                System.out.println("Enter average point.");
-                                element.setAveragePoint(Integer.parseInt(br.readLine()));
+                                    System.out.println("Enter average point.");
+                                    element.setAveragePoint(Integer.parseInt(br.readLine()));
 
-                                System.out.println("Enter one difficulty of following list:");
-                                Difficulty[] difficulties = Difficulty.values();
-                                for (Difficulty difficulty : difficulties) {
-                                    System.out.print(difficulty.toString() + "; ");
-                                }
+                                    System.out.println("Enter one difficulty of following list:");
+                                    Difficulty[] difficulties = Difficulty.values();
+                                    for (Difficulty difficulty : difficulties) {
+                                        System.out.print(difficulty.toString() + "; ");
+                                    }
 
-                                element.setDifficulty(br.readLine());
+                                    element.setDifficulty(br.readLine());
 
-                                System.out.println("Enter discipline parametrs:");
-                                Discipline discipline = new Discipline();
-                                System.out.println("Enter discipline name.");
-                                discipline.setName(br.readLine());
-                                System.out.println("Enter discipline lectureHours.");
-                                discipline.setLectureHours(Long.parseLong(br.readLine()));
-                                System.out.println("Enter discipline practiceHours.");
-                                discipline.setPracticeHours(Integer.parseInt(br.readLine()));
-                                System.out.println("Enter discipline selfStudyHours.");
-                                discipline.setSelfStudyHours(Integer.parseInt(br.readLine()));
-                                element.setDiscipline(discipline);
-                                ((ElementNeed) command).setLabWorkElement(element);
-                                commandIsReady = true;
-                                exit = true;
-                            } catch (InvalidArgumentsException e) {
-                                System.out.println("You have entered invalidate value." + e.getMessage() + "\nDo you want to exit from command? (yes/no)");
-                                if (br.readLine().equals("yes") && !filename.equals("no file")) {
+                                    System.out.println("Enter discipline parametrs:");
+                                    Discipline discipline = new Discipline();
+                                    System.out.println("Enter discipline name.");
+                                    discipline.setName(br.readLine());
+                                    System.out.println("Enter discipline lectureHours.");
+                                    discipline.setLectureHours(Long.parseLong(br.readLine()));
+                                    System.out.println("Enter discipline practiceHours.");
+                                    discipline.setPracticeHours(Integer.parseInt(br.readLine()));
+                                    System.out.println("Enter discipline selfStudyHours.");
+                                    discipline.setSelfStudyHours(Integer.parseInt(br.readLine()));
+                                    element.setDiscipline(discipline);
+                                    ((ElementNeed) command).setLabWorkElement(element);
+                                    commandIsReady = true;
                                     exit = true;
-                                    commandIsReady = false;
-                                    System.out.println("You have exit from previous command.");
-                                } else {
-                                    System.out.println("Try again.");
-                                }
-                            } catch (NumberFormatException e) {
-                                System.out.println("You have entered wrong type value." + "\nDo you want to exit from command? (yes/no)");
-                                if (br.readLine().equals("yes") || !filename.equals("no file")) {
-                                    exit = true;
-                                    commandIsReady = false;
-                                    System.out.println("You have exit from previous command.");
-                                } else {
-                                    System.out.println("Try again.");
+                                } catch (InvalidArgumentsException e) {
+                                    System.out.println("You have entered invalidate value." + e.getMessage() + "\nDo you want to exit from command? (yes/no)");
+                                    if (br.readLine().equals("yes") && !filename.equals("no file")) {
+                                        exit = true;
+                                        commandIsReady = false;
+                                        System.out.println("You have exit from previous command.");
+                                    } else {
+                                        System.out.println("Try again.");
+                                    }
+                                } catch (NumberFormatException e) {
+                                    System.out.println("You have entered wrong type value." + "\nDo you want to exit from command? (yes/no)");
+                                    if (br.readLine().equals("yes") || !filename.equals("no file")) {
+                                        exit = true;
+                                        commandIsReady = false;
+                                        System.out.println("You have exit from previous command.");
+                                    } else {
+                                        System.out.println("Try again.");
+                                    }
                                 }
                             }
                         }
+                        if (commandIsReady) {
+                            System.out.println(command.run());
+                        }
                     }
-                    if(commandIsReady){
-                        System.out.println(command.run());
+                    if (line[0].equals("exit")) {
+                        br.close();
+                        System.exit(0);
+                    } else if (line[0].equals("execute_script")) {
+                        if (line.length != 2) {
+                            System.out.println("execute_script have the only argument - filename.");
+                        } else {
+                            terminal(parser, commandMap, line[1], level + 1);
+                        }
+                    } else {
+                        System.out.println("There is no such command.");
                     }
-                }else{
-                if (line[0].equals("exit")) {
-                    br.close();
-                    System.exit(0);
-                }else if(line[0].equals("execute_script")) {
-                    if(line.length != 2){
-                        System.out.println("execute_script have the only argument - filename.");
-                    }else {
-                        terminal(parser, commandMap, line[1], level + 1);
-                    }
-                }else{
-                    System.out.println("There is no such command.");}
                 }
             } catch (InvalidArgumentsException e) {
                 System.out.println(e.getMessage());
             }
         }
+    }
+
+    private String[] cleanLine(String[] line) {
+        ArrayList<String> result = new ArrayList<>();
+        for (String s : line) {
+            if (!s.equals("")) {
+                result.add(s);
+            }
+        }
+        String[] resultStr = new String[result.size()];
+        return result.toArray(resultStr);
     }
 }
